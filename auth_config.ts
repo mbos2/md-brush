a727e9bbbefa5cb3be17b02bfe6a136d0c89e5e0
Matej Bošnjak <mbosnjak2@outlook.com>
2021-08-16 18:17:14 +0200

    added prood auth

--- auth_config.ts
+++ auth_config.ts
@@ -1,2 +1,3 @@
 export const auth0_domain = 'dev-mbos2.eu.auth0.com';
 export const auth0_client_id = 'QIqIKUrImJfrcHOBTJad1ro9Eo13ZafW';
+export const auth0_client_id_prod = '5gdsRXuGrBsX1CO98OOlDG4SivMxmWi7';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-import { auth0_client_id, auth0_domain } from "auth_config";
+import { auth0_client_id_prod, auth0_domain } from "auth_config";
 
 export const environment = {
   production: true,
   auth: {    
     domain: auth0_domain,
-    client_id: auth0_client_id,
+    client_id: auth0_client_id_prod,
   }
 };
