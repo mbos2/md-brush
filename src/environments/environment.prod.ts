16f542c612a68812822b7864367ba01533bd0d5c
Matej Bošnjak <mbosnjak2@outlook.com>
2021-08-16 18:10:17 +0200

    added env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import { auth0_client_id, auth0_domain } from "auth_config";
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {    
+    domain: auth0_domain,
+    client_id: auth0_client_id,
+  }
 };
